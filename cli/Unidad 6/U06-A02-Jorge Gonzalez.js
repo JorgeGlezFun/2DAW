0aaaf067749d594fdc20ffc26ed960c67240a8d2
Jorge Gonzalez Fuentes <jorge.gonzalez.fuentes@iesdonana.org>
2024-01-29 21:14:45 +0100

    Ejercicio 2 hecho, 3 a medias

--- cli/Unidad 6/U06-A02-Jorge Gonzalez.js
+++ cli/Unidad 6/U06-A02-Jorge Gonzalez.js
@@ -1,5 +1,18 @@
-//Crear un código en HTLM en el que tengamos un boton para añadir nodos y otro para eliminar nodos.
+// Crear un código en HTML en el que tengamos un boton para añadir nodos y otro para eliminar nodos.
+// Los nodos creados y eliminados serán elementos nuevos (<div>) del documento HTML,
+// donde se deberá escribir un texto solicitado al cliente (por ejemplo por prompt).
+// El código de crear, asi como de eliminar nodos deberá estar en JavaScript. Utilizando el manejo del DOM.
 
-//Los nodos creados y eliminados serán elementos nuevos (<div>) del documento HTML, donde se deberá escribir un texto solicitado al cliente (por ejemplo por prompt).
+var crear = document.getElementById('crear');
+crear.addEventListener('click', ()=>{
+    var contenido = prompt('Introduce el texto a introducir: ');
+    var div = document.createElement('div');
+    var texto = document.createTextNode(contenido);
+    document.body.appendChild(div);
+    div.appendChild(texto);
 
-//El código de crear, asi como de eliminar nodos deberá estar en JavaScript. Utilizando el manejo del DOM.
+    var borrar = document.getElementById('borrar');
+    borrar.addEventListener('click', ()=>{
+        document.body.removeChild(div);
+    });
+});
--- cli/Unidad 6/U06-A03-Jorge Gonzalez.js
+++ cli/Unidad 6/U06-A03-Jorge Gonzalez.js
@@ -2,3 +2,14 @@
 // a) Crear constructor con las siguientes propiedades: marca, modelo, color y año fabricación
 // b) Introducir 5 vehículos diferente.
 // c) Crear un botón para que genere una tabla (con manejo del DOM) y la visualicemos en la pagina web
+
+// a)
+function Vehiculo(marca, modelo, color, anyo){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.color = color;
+    this.anyo = anyo;
+};
+
+// b)
+
