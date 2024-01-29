30a6a818c2277ba2cd8f08b25e426beaa7a8e706
Jorge González Fuentes <jorgegonfue4d@gmail.com>
2024-01-29 16:37:58 +0100

    Enunciados ejercicios 1-5 Tema 6

---
+++ cli/Unidad 6/U06-A03-Jorge Gonzalez.js
@@ -0,0 +1,4 @@
+// Crear objeto Vehículo.
+// a) Crear constructor con las siguientes propiedades: marca, modelo, color y año fabricación
+// b) Introducir 5 vehículos diferente.
+// c) Crear un botón para que genere una tabla (con manejo del DOM) y la visualicemos en la pagina web
---
+++ cli/Unidad 6/U06-A04-Jorge Gonzalez.js
@@ -0,0 +1,3 @@
+// Crear una array multidimensional que recoja los nombres de 5 alumnos y las notas obtenidas en tres módulos.
+// Dicha información será introducida por el usuario y una vez finalizada la recogida de datos se mostrará el contenido del array en forma de tabla.
+// Tabla generada por manejo del DOM
