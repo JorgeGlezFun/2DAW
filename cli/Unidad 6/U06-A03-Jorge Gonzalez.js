828a1eda3f4cca95ab5abbaac6c30c0ba1c55578
Jorge Gonzalez Fuentes <jorge.gonzalez.fuentes@iesdonana.org>
2024-01-31 21:19:06 +0100

    Actividad 3 a medias

--- cli/Unidad 6/U06-A03-Jorge Gonzalez.js
+++ cli/Unidad 6/U06-A03-Jorge Gonzalez.js
@@ -12,4 +12,32 @@ function Vehiculo(marca, modelo, color, anyo){
 };
 
 // b)
+var coches = {
+    "coche1": new Vehiculo('Hyundai', 'i30', 'Blanco', '2008'),
+    "coche2": new Vehiculo('Audi', 'R8', 'Rojo', '2017'),
+    "coche3": new Vehiculo('Volkswagen', 'Passat', 'Negro', '2020'),
+    "coche4": new Vehiculo('Chevrolet', 'Epica', 'Champagne', '2008'),
+    "coche5": new Vehiculo('Mercedes', 'AMG', 'Plata', '2024')
+}
 
+var longitud = coches.length;
+const valoresObjeto = ['marca',]
+// c)
+var tabla = document.createElement('table');
+var cuerpoTabla = document.createElement('tbody');
+var columna = document.createElement('td');
+  // Crea las celdas
+for (var i = 0; i < longitud; i++) {
+// Crea las hileras de la tabla
+    var fila = document.createElement("tr");
+    var columna = document.createElement("td");
+    var textoColumna = document.createTextNode(
+        coches['coche' + i].marca
+        );
+    columna.appendChild(textoColumna);
+    fila.appendChild(columna);
+    cuerpoTabla.appendChild(fila);
+}
+tabla.appendChild(cuerpoTabla);
+document.appendChild(tabla);
+tabla.setAttribute("border", "2");
