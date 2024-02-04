404ca121f39795f210e77953cc689f2dd21c2ad5
jorgeglezfun <jorgegonfue@gmail.com>
2024-02-04 22:09:00 +0100

    Ejercicio 5 a medias

--- cli/Unidad 6/U06-A05-Jorge Gonzalez.js
+++ cli/Unidad 6/U06-A05-Jorge Gonzalez.js
@@ -1,5 +1,22 @@
 /* Realizar un código en el que tengamos en HTML varios párrafos con un determinado texto escrito.
+Crear un botón para ocultar/visualizar el párrafo elegido.
+Para ello puedes utilizar dos clases en CSS y, mediante JavaScript, se irá
+cambiando la clase al elemento del DOM */
+var parrafos = document.getElementById('parrafos').value;
+var parrafo1 = document.getElementById('parrafo1');
+var parrafo2 = document.getElementById('parrafo1');
+var parrafo3 = document.getElementById('parrafo1');
+var boton = document.getElementById('boton');
+var booleano = false;
 
-Crear un botón para ocultar/visualizar el párrafo  elegido.
+function escondite(parrafo){
+    if (booleano) {
+        parrafo.className = 'escondido';
+        booleano = false;
+    } else {
+        parrafo.className = '';
+        booleano = true;
+    }
+}
 
-Para ello puedes utilizar dos clases en CSS y, mediante JavaScript, se irá cambiando la clase al elemento del DOM */
+boton.addEventListener('click', escondite(parrafos));
